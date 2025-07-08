9943872b87625d85b73436ab2c15eb5d14634891
ian otieno <ianotieno23@gmail.com>
2025-07-08 17:29:04 +0300

    update

--- lib/Screen/Home.dart
+++ lib/Screen/Home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 import 'package:rentit/Screen/HomePage.dart';
 
--- lib/Screen/login.dart
+++ lib/Screen/login.dart
@@ -13,7 +13,7 @@ class Login extends StatelessWidget {
                 Icon(
                   Icons.lock,
                   size: 100,
-                  color: Colors.blue,
+                  color: const Color.fromARGB(255, 11, 11, 11),
                 ),
                 const SizedBox(height: 50),
                 Text("Welcome Back you\'ve been missed",
@@ -33,19 +33,43 @@ class Login extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 20),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    
-                    prefixIcon: Icon(Icons.email),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue, width: 2.0),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                    ),
-                  ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
                   
+                      prefixIcon: Icon(Icons.email),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black12,  width: 2.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                    ),
+                    
+                  ),
+                ),
+                 Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                  
+                      prefixIcon: Icon(Icons.email),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black12,  width: 2.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                    ),
+                    
+                  ),
                 ),
             ],
         ),
